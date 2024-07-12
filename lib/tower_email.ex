483a27ca92e1e7d7b481834ea63c7b18f567ca8b
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-07-12 16:38:06 -0300

    mix new tower_email

---
+++ lib/tower_email.ex
@@ -0,0 +1,18 @@
+defmodule TowerEmail do
+  @moduledoc """
+  Documentation for `TowerEmail`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TowerEmail.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tower_email_test.exs
@@ -0,0 +1,8 @@
+defmodule TowerEmailTest do
+  use ExUnit.Case
+  doctest TowerEmail
+
+  test "greets the world" do
+    assert TowerEmail.hello() == :world
+  end
+end
