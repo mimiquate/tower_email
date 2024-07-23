32ad63ac2c723721446c45e25a3a6de8f9cf68f1
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-07-23 14:53:14 -0300

    removes unused function

--- lib/tower_email.ex
+++ lib/tower_email.ex
@@ -2,17 +2,4 @@ defmodule TowerEmail do
   @moduledoc """
   Documentation for `TowerEmail`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TowerEmail.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
