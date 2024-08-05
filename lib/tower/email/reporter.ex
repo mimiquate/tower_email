7005a84ef7194032e03cd5a81824a72b1b95c06c
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-08-05 11:32:44 -0300

    feat: includes event id in message body

--- lib/tower/email/reporter.ex
+++ lib/tower/email/reporter.ex
@@ -2,19 +2,19 @@ defmodule Tower.Email.Reporter do
   @behaviour Tower.Reporter
 
   @impl true
-  def report_event(%Tower.Event{kind: :error, reason: exception, stacktrace: stacktrace}) do
-    send_email(inspect(exception.__struct__), Exception.message(exception), stacktrace)
+  def report_event(%Tower.Event{kind: :error, id: id, reason: exception, stacktrace: stacktrace}) do
+    send_email(id, inspect(exception.__struct__), Exception.message(exception), stacktrace)
   end
 
-  def report_event(%Tower.Event{kind: :throw, reason: reason, stacktrace: stacktrace}) do
-    send_email("Uncaught throw", reason, stacktrace)
+  def report_event(%Tower.Event{kind: :throw, id: id, reason: reason, stacktrace: stacktrace}) do
+    send_email(id, "Uncaught throw", reason, stacktrace)
   end
 
-  def report_event(%Tower.Event{kind: :exit, reason: reason, stacktrace: stacktrace}) do
-    send_email("Exit", reason, stacktrace)
+  def report_event(%Tower.Event{kind: :exit, id: id, reason: reason, stacktrace: stacktrace}) do
+    send_email(id, "Exit", reason, stacktrace)
   end
 
-  def report_event(%Tower.Event{kind: :message, level: level, reason: message}) do
+  def report_event(%Tower.Event{kind: :message, id: id, level: level, reason: message}) do
     m =
       if is_binary(message) do
         message
@@ -22,12 +22,12 @@ defmodule Tower.Email.Reporter do
         inspect(message)
       end
 
-    send_email("[#{level}] #{m}", "")
+    send_email(id, "[#{level}] #{m}", "")
   end
 
-  defp send_email(kind, reason, stacktrace \\ nil) do
+  defp send_email(id, kind, reason, stacktrace \\ nil) do
     {:ok, _} =
-      Tower.Email.Message.new(kind, reason, stacktrace)
+      Tower.Email.Message.new(id, kind, reason, stacktrace)
       |> Tower.Email.Mailer.deliver()
 
     :ok
