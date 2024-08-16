3ced522473d0d85b463069e3f126093b1d58440c
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-08-16 15:51:06 -0300

    build(deps): bumps tower from 0.2.0 to 0.4.0

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule TowerEmail.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:tower, "~> 0.2.0"},
+      {:tower, "~> 0.4.0"},
       {:swoosh, "~> 1.14"},
 
       # Optional
