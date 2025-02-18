da6fd6a17a32fdf954b993ff7d6426bb45d94be8
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-02-18 15:22:32 -0300

    feat: better metadata

--- lib/tower_email/reporter.ex
+++ lib/tower_email/reporter.ex
@@ -9,23 +9,50 @@ defmodule TowerEmail.Reporter do
     end
   end
 
-  defp do_report_event(%Tower.Event{kind: kind, id: id, reason: reason, stacktrace: stacktrace})
+  defp do_report_event(%Tower.Event{
+         kind: kind,
+         id: id,
+         reason: reason,
+         stacktrace: stacktrace,
+         similarity_id: similarity_id,
+         datetime: datetime
+       })
        when kind in [:error, :throw, :exit] do
+    similarity_id = fixed_length_similarity_id(similarity_id)
+
     send_email(
-      id,
+      similarity_id,
       Exception.format_banner(kind, reason, stacktrace),
-      Exception.format(kind, reason, stacktrace)
+      Exception.format(kind, reason, stacktrace),
+      id: id,
+      similarity_id: similarity_id,
+      datetime: datetime
     )
   end
 
-  defp do_report_event(%Tower.Event{kind: :message, id: id, level: level, reason: reason}) do
+  defp do_report_event(%Tower.Event{
+         kind: :message,
+         id: id,
+         level: level,
+         reason: reason,
+         similarity_id: similarity_id,
+         datetime: datetime
+       }) do
     message = "[#{level}] #{if(is_binary(reason), do: reason, else: inspect(reason))}"
+    similarity_id = fixed_length_similarity_id(similarity_id)
 
-    send_email(id, message, message)
+    send_email(
+      similarity_id,
+      message,
+      message,
+      id: id,
+      similarity_id: similarity_id,
+      datetime: datetime
+    )
   end
 
-  defp send_email(id, title, body) do
-    email_message = TowerEmail.Message.new(id, title, body)
+  defp send_email(similarity_id, title, body, metadata) do
+    email_message = TowerEmail.Message.new(similarity_id, title, body, metadata)
 
     async(fn ->
       {:ok, _} = TowerEmail.Mailer.deliver(email_message)
@@ -38,6 +65,10 @@ defmodule TowerEmail.Reporter do
     # This config env can be to any of the 8 levels in https://www.erlang.org/doc/apps/kernel/logger#t:level/0,
     # or special values :all and :none.
     Application.get_env(:tower_email, :level, @default_level)
+  end
+
+  defp fixed_length_similarity_id(number) do
+    String.pad_leading(to_string(number), 10, "0")
   end
 
   defp async(fun) do
